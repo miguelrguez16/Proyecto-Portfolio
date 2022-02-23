11938dd59ec0b8f1dfc1736340db4234bc5f5186
Miguel <uo263494@uniovi.es>
2022-02-23 22:18:18 +0100

    [ngStyle] exportar clases

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-
+import { configuracion } from './models/configuracion';
 @Component({
   selector: 'app-root', // va a la etiqueta del html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title:string = 'Angular Inicio';
+  public title:string = 'Angular';
+  public descripcion:string ="";
+  public fondo:string="";
+  public color:string="";
   public mostrar_videojuegos:boolean = true;
 
+  constructor(){
+    this.title = configuracion.titulo;
+    this.descripcion = configuracion.descripcion;
+    this.color =configuracion.color;
+    this.fondo=configuracion.fondo;
+  }
   public setEnabled(valor:boolean):void{
     this.mostrar_videojuegos = valor;
   }
---
+++ src/app/models/configuracion.ts
@@ -0,0 +1,11 @@
+
+
+
+export var configuracion = {
+  color: 'white',
+  fondo: 'black',
+  titulo: 'Miguel Rodríguez González',
+  descripcion: 'Aprendiendo algo de angular y más',
+};
+
+
