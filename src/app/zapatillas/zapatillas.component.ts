92e40e719377716d98ff8486e201cfc95abd604e
Miguel <uo263494@uniovi.es>
2022-02-24 11:01:44 +0100

    directiva ngIf y etiqueta style y ternaria

--- src/app/zapatillas/zapatillas.component.ts
+++ src/app/zapatillas/zapatillas.component.ts
@@ -2,24 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import { Zapatilla } from '../models/zapatilla';
 
 @Component({
-    selector: "zapatillas",
-    templateUrl: "./zapatillas.component.html"
+  selector: 'zapatillas',
+  templateUrl: './zapatillas.component.html',
 })
+export class ZapatillasComponent implements OnInit {
+  public titulo: string;
+  public zapas: Array<Zapatilla>;
 
-export class ZapatillasComponent implements OnInit{
-    public titulo:string;
-    public zapas:Array<Zapatilla>;
-
-    constructor(){
-        this.titulo = "Componente de Zapatillas";
-        this.zapas = [
-            new Zapatilla("Reebok Classic", "Reebok",60,"azul",true),
-            new Zapatilla("Nike Classic", "Nike",80,"white",false),
-            new Zapatilla("Fila Classic", "Fila",90,"green",true)
-        ];
-        console.log("Cargado Zapatillas");
-    }
-    ngOnInit(): void {
-        console.table(this.zapas);
-    }
+  constructor() {
+    this.titulo = 'Componente de Zapatillas';
+    this.zapas = [
+      new Zapatilla('Adidas Classic', 'Adidas', 110, 'green', true),
+      new Zapatilla('Reebok Classic', 'Reebok', 60, 'azul', true),
+      new Zapatilla('Nike Classic', 'Nike', 75, 'white', false),
+      new Zapatilla('Fila Classic', 'Fila', 90, 'green', true),
+    ];
+    console.log('Cargado Zapatillas');
+  }
+  ngOnInit(): void {
+    console.table(this.zapas);
+  }
 }
