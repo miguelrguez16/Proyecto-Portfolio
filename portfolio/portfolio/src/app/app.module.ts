b1f10bde5ec647f03a2a88fa30ac8317e8422b9a
Miguel <uo263494@uniovi.es>
2022-03-01 10:30:36 +0100

    creacion de componentes proyecto

--- portfolio/portfolio/src/app/app.module.ts
+++ portfolio/portfolio/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    CreateComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
