99d74617b55386639025aae6e2dbb03a4519f1d0
Miguel <uo263494@uniovi.es>
2022-03-03 21:04:19 +0100

    listado de proyectos

--- portfolio/portfolio/src/app/components/projects/projects.component.ts
+++ portfolio/portfolio/src/app/components/projects/projects.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Project } from 'src/app/models/project.model';
+import { Global } from 'src/app/services/global';
+import { ProjectService } from 'src/app/services/project.service';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  styleUrls: ['./projects.component.css'],
+  providers: [ProjectService]
 })
 export class ProjectsComponent implements OnInit {
+   public _projects:Project[]=[];
+   public _url:String;
 
-  constructor() { }
+
+  constructor(
+    private _projectService: ProjectService
+  ) { 
+      this._url = Global.url;
+  }
 
   ngOnInit(): void {
+    this.getProjects();
+  }
+
+  getProjects(){
+    this._projectService.getProjects().subscribe(
+      response =>{
+        this._projects = response.projects;
+        console.table(response.projects);
+      },err=>{console.error(err)}
+    );
   }
 
 }
