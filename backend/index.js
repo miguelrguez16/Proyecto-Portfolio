c1c02c4849134841282919dc8d6a585324ade6e3
Miguel Rguez <uo263494@uniovi.es>
2022-05-22 19:10:56 +0200

    fix: backend env

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,9 @@
 "use strict";
+const dotenv = require('dotenv').config();
+const passwordMONGO = process.env.MONGO;
 // conexion base de datos
 var mongoose = require("mongoose");
-var dbUri = "mongodb+srv://miguelucu16:C28AgNHSarJza79l@eol.rp9fe.mongodb.net/PortFolioBackend?retryWrites=true&w=majority";
-
+var dbUri = "mongodb+srv://miguelucu16:" + passwordMONGO + "@eol.rp9fe.mongodb.net/PortFolioBackend?retryWrites=true&w=majority"
 //creacion servidor
 var app = require("./app");
 var port = 8080;
@@ -11,12 +12,12 @@ mongoose.Promise = global.Promise;
 mongoose
   .connect(dbUri)
   .then(() => {
-    console.log("BBDD: en linea...");
+    console.log("\tBBDD UP...");
 
     app.listen(port, () => {
-      console.log("APP: en linea...");
-      console.log("--> localhost:8080");
+      console.log("\tAPP: en linea...");
+      console.log("\t--> localhost:8080");
 
     });
   })
-  .catch((err) => console.log(err));
+  .catch((err) => console.error(err));
